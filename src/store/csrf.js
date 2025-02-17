8dd711f963465360fd4366bb3434bfc72a9cbbf8
Muhammad Murtaza Hassan <samri.berih@gmail.com>
2025-02-17 15:07:22 +0500

    Update csrf.js

--- src/store/csrf.js
+++ src/store/csrf.js
@@ -10,30 +10,7 @@ export async function csrfFetch(url, options = {}) {
     options.headers["Content-Type"] = options.headers['Content-Type'] || "application/json";
     options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
   }
-
-  //--------------------------------------------------------
-  const csrfToken = Cookies.get("XSRF-TOKEN");
-  console.log("csrfToken", csrfToken);
   
-// If the CSRF token is not available, fetch it
-if (!csrfToken && !isFetchingCsrfToken) {
-  console.log("CSRF token not found, fetching...");
-  isFetchingCsrfToken = true; // Set the flag to indicate we're fetching the token
-
-  try {
-    const restoreResponse = await restoreCSRF();
-    if (restoreResponse.ok) {
-      // Retrieve the CSRF token again after restoring
-      options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
-    } else {
-      throw new Error("Failed to restore CSRF token");
-    }
-  } finally {
-    isFetchingCsrfToken = false; // Reset the flag after fetching
-  }
-}
-  //--------------------------------------------------------
-
   console.log("url", url, "options", options);
   const response = await window.fetch(url, options);
 
