3edd750536412e7f5f55d0e1987849d61b7e6696
samri berih <samri.berih@gmail.com>
2025-02-18 06:48:13 +0500

    Revert "Update store.js"
    This reverts commit c19084465557b511f0b27cfbf1f0c8de055a95d7.

--- src/store/store.js
+++ src/store/store.js
@@ -12,14 +12,14 @@ const rootReducer = combineReducers({
 });
 
 let enhancer;
-// if (import.meta.env.MODE === "production") {
-//   enhancer = applyMiddleware(thunk);
-// } else {
+if (import.meta.env.MODE === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
   const logger = (await import("redux-logger")).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
+}
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
