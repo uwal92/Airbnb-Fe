6c4902805d3efa7a4aa5672aa95cf1164a18564a
samri berih <samri.berih@gmail.com>
2025-02-18 06:51:38 +0500

    Update csrf.js

--- src/store/csrf.js
+++ src/store/csrf.js
@@ -5,16 +5,15 @@ let isFetchingCsrfToken = false; // Flag to prevent multiple fetch calls
 export async function csrfFetch(url, options = {}) {
   options.headers = options.headers || {};
   options.method = options.method || "GET";
-  options.credentials = "include";
+  //options.credentials = "include";
 
   if (options.method && options.method.toUpperCase() !== "GET") {
     options.headers["Content-Type"] = options.headers['Content-Type'] || "application/json";
-    options.headers["XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
+    options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
     console.log('  ---  cookies ---', Cookies.get())
   }
 
   console.log("url", url, "options", options);
-  // const response1 = await window.fetch("https://airbnb-api-docs.onrender.com", options);
   const response = await window.fetch(url, options);
 
   if (response.status >= 400) throw response;
