4f7ac2ebb83573ee12c2c557c37d8d6ad12daa8d
samri berih <samri.berih@gmail.com>
2025-02-18 06:27:37 +0500

    Update csrf.js

--- src/store/csrf.js
+++ src/store/csrf.js
@@ -9,7 +9,7 @@ export async function csrfFetch(url, options = {}) {
 
   if (options.method && options.method.toUpperCase() !== "GET") {
     options.headers["Content-Type"] = options.headers['Content-Type'] || "application/json";
-    options.headers["XSRF-TOKEN"] = Cookies.get("_csrf");
+    options.headers["XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
     console.log('  ---  cookies ---', Cookies.get())
   }
 
